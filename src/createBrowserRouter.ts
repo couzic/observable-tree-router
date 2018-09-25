41a051fe5a007be6d7c9d677e431b590318eac8d
couzic <mikaelcouzic@gmail.com>
2018-09-25 10:22:07 +0200

    Remove dead comments

--- src/createBrowserRouter.ts
+++ src/createBrowserRouter.ts
@@ -117,6 +117,7 @@ class NestedBrowserRouter {
          i++
       ) {
          if (hasMatched !== null) {
+            // TODO
             //  this._nestedRouters[i]._unmatch()
          } else {
             hasMatched = this._nestedRouters[i]._testUrl(url)
@@ -145,44 +146,6 @@ class NestedBrowserRouter {
          }
       } else return null
    }
-   //    private _handlePushWhenMatchingExact(params: any) {
-   //       if (params === undefined) return
-   //       if (
-   //          this._params !== undefined &&
-   //          equalParams(this._params, this.currentParams, params)
-   //       )
-   //          return
-   //       const newState = {
-   //          ...this.currentState,
-   //          match: { exact: true, params }
-   //       }
-   //       this._parentRouter._onChildMatchAgain(this._routeId, newState)
-   //       this._state$.next(newState)
-   //       this._match$.next(newState.match)
-   //    }
-   //    private _handlePushWhenMatchingChild(params: any) {
-   //       this._unmatchChildren()
-   //       const newState = params
-   //          ? {
-   //               match: { exact: true, params },
-   //               ...this._retrieveNestedRouteStates()
-   //            }
-   //          : { match: { exact: true }, ...this._retrieveNestedRouteStates() }
-   //       this._parentRouter._onChildMatchAgain(this._routeId, newState)
-   //       this._state$.next(newState)
-   //       this._match$.next(newState.match)
-   //    }
-   //    private _handlePushWhenNotMatching(params: any) {
-   //       const newState = params
-   //          ? {
-   //               match: { exact: true, params },
-   //               ...this._retrieveNestedRouteStates()
-   //            }
-   //          : { match: { exact: true }, ...this._retrieveNestedRouteStates() }
-   //       this._parentRouter._onChildMatch(this._routeId, newState)
-   //       this._state$.next(newState)
-   //       this._match$.next(newState.match)
-   //    }
    private _retrieveNestedRouteStates() {
       const nestedStates = {} as any
       this._nestedRouteIds.forEach(nestedRouteId => {
@@ -190,127 +153,6 @@ class NestedBrowserRouter {
       })
       return nestedStates
    }
-   //    /**
-   //     * Called when a direct child now matches, and previously did not.
-   //     */
-   //    private _onChildMatch(routeId: string, newChildState: any) {
-   //       if (this.isMatchingExact) {
-   //          this._handleChildMatchWhenMatchingExact(routeId, newChildState)
-   //       } else if (this.isMatchingChild) {
-   //          this._handleChildMatchWhenMatchingOtherChild(routeId, newChildState)
-   //       } else {
-   //          this._handleChildMatchWhenNotMatching(routeId, newChildState)
-   //       }
-   //    }
-   //    private _handleChildMatchWhenMatchingExact(
-   //       routeId: string,
-   //       newChildState: any
-   //    ) {
-   //       const newState = {
-   //          ...this.currentState,
-   //          [routeId]: newChildState
-   //       } as any
-   //       if (this._params === undefined) {
-   //          newState.match = { exact: false }
-   //       } else {
-   //          const newParams = {} as any
-   //          this._params.forEach((param: string) => {
-   //             newParams[param] = newChildState.match.params[param]
-   //          })
-   //          newState.match = { exact: false, params: newParams }
-   //       }
-   //       this._parentRouter._onChildMatchAgain(this._routeId, newState)
-   //       this._state$.next(newState)
-   //       this._match$.next(newState.match)
-   //    }
-   //    private _handleChildMatchWhenMatchingOtherChild(
-   //       routeId: string,
-   //       newChildState: any
-   //    ) {
-   //       this._unmatchChildren()
-   //       const newState = {
-   //          match: this.currentState.match,
-   //          ...this._retrieveNestedRouteStates(),
-   //          [routeId]: newChildState
-   //       } as any
-   //       if (this._params === undefined) {
-   //          this._parentRouter._onChildStateChanged(this._routeId, newState)
-   //          this._state$.next(newState)
-   //       } else {
-   //          const newParams = {} as any
-   //          this._params.forEach((param: string) => {
-   //             newParams[param] = newChildState.match.params[param]
-   //          })
-   //          if (equalParams(this._params, this.currentParams, newParams)) {
-   //             this._parentRouter._onChildStateChanged(this._routeId, newState)
-   //             this._state$.next(newState)
-   //          } else {
-   //             newState.match = { exact: false, params: newParams }
-   //             this._parentRouter._onChildMatchAgain(this._routeId, newState)
-   //             this._state$.next(newState)
-   //             this._match$.next(newState.match)
-   //          }
-   //       }
-   //    }
-   //    private _handleChildMatchWhenNotMatching(
-   //       routeId: string,
-   //       newChildState: any
-   //    ) {
-   //       const newState = {
-   //          ...this.currentState,
-   //          [routeId]: newChildState
-   //       } as any
-   //       if (this._params === undefined) {
-   //          newState.match = { exact: false }
-   //       } else {
-   //          const newParams = {} as any
-   //          this._params.forEach((param: string) => {
-   //             newParams[param] = newChildState.match.params[param]
-   //          })
-   //          newState.match = { exact: false, params: newParams }
-   //       }
-   //       this._parentRouter._onChildMatch(this._routeId, newState)
-   //       this._state$.next(newState)
-   //       this._match$.next(newState.match)
-   //    }
-   //    /**
-   //     * Called when a route change causes a child to update its state, but since is not directly concerned by the change it does not emit a new match.
-   //     * From this point and down to root store, params can not change.
-   //     */
-   //    private _onChildStateChanged(routeId: string, newChildState: any) {
-   //       const newState = {
-   //          ...this.currentState,
-   //          [routeId]: newChildState
-   //       } as any
-   //       this._parentRouter._onChildStateChanged(this._routeId, newState)
-   //       this._state$.next(newState)
-   //    }
-   //    /**
-   //     * Called when a child that previously matched receives new and different params
-   //     */
-   //    private _onChildMatchAgain(routeId: string, newChildState: any) {
-   //       if (this._params === undefined) {
-   //          this._onChildStateChanged(routeId, newChildState)
-   //       } else {
-   //          const newState = {
-   //             ...this.currentState,
-   //             [routeId]: newChildState
-   //          } as any
-   //          const newParams = {} as any
-   //          this._params.forEach((param: string) => {
-   //             newParams[param] = newChildState.match.params[param]
-   //          })
-   //          if (equalParams(this._params, this.currentParams, newParams)) {
-   //             this._parentRouter._onChildStateChanged(this._routeId, newState)
-   //             this._state$.next(newState)
-   //          } else {
-   //             newState.match = { exact: false, params: newParams }
-   //             this._parentRouter._onChildMatchAgain(this._routeId, newState)
-   //             this._state$.next(newState)
-   //             this._match$.next(newState.match)
-   //          }
-   //       }
-   //    }
    private _unmatch() {
       if (this.isMatching) {
          this._unmatchChildren()
@@ -398,23 +240,5 @@ export function createBrowserRouter<Config extends RouterConfig>(
          router._state$.next(newState)
       }
    })
-   //    router._onChildMatch = (childId: string, newChildState: any) => {
-   //       router._unmatchAll()
-   //       const newState = {} as any
-   //       routeIds.forEach(routeId => {
-   //          newState[routeId] = router[routeId].currentState
-   //       })
-   //       newState[childId] = newChildState
-   //       router._state$.next(newState)
-   //    }
-   //    router._onChildStateChanged = (childId: string, newChildState: any) => {
-   //       const newState = {
-   //          ...router.currentState,
-   //          [childId]: newChildState
-   //       }
-   //       router._state$.next(newState)
-   //    }
-   //    router._onChildMatchAgain = router._onChildStateChanged
-   //    router._unmatchAll = () => routeIds.forEach(id => router[id]._unmatch())
    return router
 }
